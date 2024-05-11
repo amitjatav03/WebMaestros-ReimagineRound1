57ea0cb285755984b78e5ff7859cd85ab0433dcc
amitjatav03 <jatavamit0311@gmail.com>
2024-05-11 18:00:13 +0530

    marquee-effect

--- script.js
+++ script.js
@@ -18,26 +18,26 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 
-Shery.mouseFollower({
-  //Parameters are optional.
-  skew: true,
-  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  duration: 1,
-});
+  Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
 
 
-Shery.imageMasker("img" /* Element to target.*/, {
-  //Parameters are optional.
-  mouseFollower: true,
-  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  duration: 1,
-});
+// Shery.imageMasker("img" /* Element to target.*/, {
+//   //Parameters are optional.
+//   mouseFollower: true,
+//   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//   duration: 1,
+// });
 
 
-Shery.hoverWithMediaCircle("img" /* Element to target.*/, {
-  images: ["images/img-1-apexlegends.jpg", "images/img-2-manorlords.jpg", "images/img-3-vrising.jpg", "images/img-4-1361526.jpeg"] /*OR*/,
-  //videos: ["video1.mp4", "video2.mp4"],
-});
+// Shery.hoverWithMediaCircle("img" /* Element to target.*/, {
+//   images: ["images/img-1-apexlegends.jpg", "images/img-2-manorlords.jpg", "images/img-3-vrising.jpg", "images/img-4-1361526.jpeg"] /*OR*/,
+//   //videos: ["video1.mp4", "video2.mp4"],
+// });
 
 
 gsap.to("#second-main", {
@@ -45,7 +45,6 @@ gsap.to("#second-main", {
   scrollTrigger: {
     scroller: "body",
     trigger: "#second-main",
-    markers: true,
     start: "top 95%",
     end: "top 10%",
     scrub: 1
@@ -64,3 +63,4 @@ apexImg.addEventListener("mouseenter", function(){
 apexImg.addEventListener("mouseout", function(){
   apexTitle.style.display = "none";
 })
+
