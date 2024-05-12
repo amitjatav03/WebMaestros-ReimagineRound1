f324c27e7761aff06f3bd74282103362d75939b7
amitjatav03 <jatavamit0311@gmail.com>
2024-05-12 14:31:10 +0530

    little-responsive

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var swiper = new Swiper(".mySwiper", {
+function landingPageSlider(){
+  var swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
@@ -7,7 +8,7 @@ var swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     coverflowEffect: {
       rotate: 70,
-      stretch: 0,
+      stretch: 30,
       depth: 200,
       modifier: 1,
       slideShadows: true,
@@ -15,15 +16,42 @@ var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
     },
-  });
+});
+}
+
+landingPageSlider();
 
 
-  Shery.mouseFollower({
-    //Parameters are optional.
-    skew: true,
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
-  });
+function sliderContent(){
+  let sliderWrapper = document.querySelector(".swiper-wrapper");
+  let sliderImages = [
+    {img: "images/img-1-apexlegends.jpg", title: "APEX LEGENDS"},
+    {img: "images/img-2-manorlords.jpg", title: "MANOR LORDS"},
+    {img: "images/img-3-vrising.jpg", title: "VRISING"},
+    {img: "images/img-4-1361526.jpeg", title: "GRAY ZONE"},
+  ]
+  
+  sliderImages.forEach(function(slide){
+    let swiperSlide = document.createElement("div");
+    swiperSlide.classList.add("swiper-slide");
+    swiperSlide.innerHTML = `
+        <img src="${slide.img}"/>
+        <h1>${slide.title}</h1>
+    `;
+    sliderWrapper.appendChild(swiperSlide);
+  })
+}
+
+
+
+
+
+Shery.mouseFollower({
+  //Parameters are optional.
+  skew: true,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
 
 
 // Shery.imageMasker("img" /* Element to target.*/, {
@@ -64,3 +92,79 @@ apexImg.addEventListener("mouseout", function(){
   apexTitle.style.display = "none";
 })
 
+
+
+
+
+
+// FOOTER ANIMATION
+
+function footerAnime(){
+  var footerSteam = document.querySelector(".footer-title");
+  let clutter = "";
+  let footerTitle = footerSteam.textContent;
+  for(let i = 0; i<footerTitle.length; i++) {
+    clutter += `<span>${footerTitle[i]}</span>`;
+  }
+  footerSteam.innerHTML = clutter;
+  
+  
+  gsap.from(".footer-title span", {
+    top: "-350px",
+    duration: .8,
+    stagger: .1,
+    ease: "poweri.out",
+    scrollTrigger: {
+      scroller: "body",
+      trigger: ".footer-bottom",
+      // markers: true,
+      start: "top 85%",
+      end: "top 82%",
+      scrub: 3,
+    }
+  })
+  
+  
+  
+  gsap.from(".btm-line, .top-line", {
+    width: 0,
+    duration: 1,
+    delay: .5,
+    scrollTrigger: {
+      scroller: "body",
+      trigger: ".f-mid",
+      start: "top 60%",
+      end: "top 50%",
+      scrub: 1,
+    }
+  })
+  
+  gsap.from(".valve-section", {
+    opacity: 0,
+    scrollTrigger: {
+      scroller: "body",
+      trigger: ".f-mid",
+      start: "top 50%",
+      end: "top 40%",
+      scrub: 1,
+    }
+  })
+  
+  
+  gsap.from('.footer-top', {
+    y: -100,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+      scroller: "body",
+      trigger: ".footer-top",
+      start: "top 50%",
+      end: "top 45%",
+      scrub: 1,
+    }
+  })
+}
+
+
+
+footerAnime();
