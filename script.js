dddd1fbfa2654c88319dd296f3dc6426c32d3bbf
amitjatav03 <jatavamit0311@gmail.com>
2024-05-26 16:51:37 +0530

    gift cards added with few changes

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
 
 (function () {
   const locomotiveScroll = new LocomotiveScroll({
-    el: document.querySelector(".front-page"),
+    el: document.querySelector(".main"),
     smooth: true,
-    direction: 'horizontal'
+    lerp: 0.2,
   });
 })();
 
+function loco() {
+  function init() {
+    gsap.registerPlugin(ScrollTrigger);
+  
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+  
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight
+        };
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+  
+  
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+  
+  }
+  
+  init()
+}
 
-// swiper-slide-fully-visible
+
+
 
 function landingPageSlider(){
   var swiper = new Swiper(".mySwiper", {
@@ -30,7 +72,6 @@ function landingPageSlider(){
     },
 });
 }
-
 landingPageSlider();
 
 
@@ -53,114 +94,97 @@ function sliderContent(){
     sliderWrapper.appendChild(swiperSlide);
   })
 }
+sliderContent();
+
+
+
+function sheryJs(){
+  Shery.mouseFollower({
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: .1,
+  });
+  
+  
+  Shery.imageMasker(".swiper-slide img" , {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+}
+sheryJs();
 
 
 
 
-Shery.mouseFollower({
-  //Parameters are optional.
-  skew: true,
-  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+
+
+function footerAnimation(){
+  var footerSteam = document.querySelector(".footer-title");
+  let clutter = "";
+  let footerTitle = footerSteam.textContent;
+  for(let i = 0; i<footerTitle.length; i++) {
+  clutter += `<span>${footerTitle[i]}</span>`;
+  }
+  footerSteam.innerHTML = clutter;
+  
+  
+  gsap.from(".footer-title span", {
+  top: "-350px",
+  duration: .8,
+  stagger: .1,
+  ease: "poweri.out",
+  scrollTrigger: {
+    scroller: "body",
+    trigger: ".footer-bottom",
+    start: "top 85%",
+    end: "top 82%",
+    scrub: 3,
+  }
+  })
+  
+  
+  
+  gsap.from(".btm-line, .top-line", {
+  width: 0,
   duration: 1,
-});
-
-
-Shery.imageMasker(".swiper-slide img" /* Element to target.*/, {
-  //Parameters are optional.
-  mouseFollower: true,
-  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  delay: .5,
+  scrollTrigger: {
+    scroller: "body",
+    trigger: ".f-mid",
+    start: "top 60%",
+    end: "top 50%",
+    scrub: 1,
+  }
+  })
+  
+  gsap.from(".valve-section", {
+  opacity: 0,
+  scrollTrigger: {
+    scroller: "body",
+    trigger: ".f-mid",
+    start: "top 50%",
+    end: "top 40%",
+    scrub: 1,
+  }
+  })
+  
+  
+  gsap.from('.footer-top', {
+  y: -100,
+  opacity: 0,
   duration: 1,
-});
-
-
-Shery.hoverWithMediaCircle("img" /* Element to target.*/, {
-  images: ["images/img-1-apexlegends.jpg", "images/img-2-manorlords.jpg", "images/img-3-vrising.jpg", "images/img-4-1361526.jpeg"] /*OR*/,
-  //videos: ["video1.mp4", "video2.mp4"],
-});
-
-
-// gsap.to(".second-main", {
-//   top: 0,
-//   scrollTrigger: {
-//     scroller: "body",
-//     trigger: ".second-main",
-//     start: "top 95%",
-//     end: "top 10%",
-//     scrub: 1
-//   }
-// })
-
-
-
-
-
-
-
-
-// FOOTER ANIMATION
-
-var footerSteam = document.querySelector(".footer-title");
-let clutter = "";
-let footerTitle = footerSteam.textContent;
-for(let i = 0; i<footerTitle.length; i++) {
-clutter += `<span>${footerTitle[i]}</span>`;
+  scrollTrigger: {
+    scroller: "body",
+    trigger: ".footer-top",
+    start: "top 50%",
+    end: "top 45%",
+    scrub: 1,
+  }
+  })
 }
-footerSteam.innerHTML = clutter;
+footerAnimation();
 
 
-gsap.from(".footer-title span", {
-top: "-350px",
-duration: .8,
-stagger: .1,
-ease: "poweri.out",
-scrollTrigger: {
-  scroller: "body",
-  trigger: ".footer-bottom",
-  start: "top 85%",
-  end: "top 82%",
-  scrub: 3,
-}
-})
-
-
-
-gsap.from(".btm-line, .top-line", {
-width: 0,
-duration: 1,
-delay: .5,
-scrollTrigger: {
-  scroller: "body",
-  trigger: ".f-mid",
-  start: "top 60%",
-  end: "top 50%",
-  scrub: 1,
-}
-})
-
-gsap.from(".valve-section", {
-opacity: 0,
-scrollTrigger: {
-  scroller: "body",
-  trigger: ".f-mid",
-  start: "top 50%",
-  end: "top 40%",
-  scrub: 1,
-}
-})
-
-
-gsap.from('.footer-top', {
-y: -100,
-opacity: 0,
-duration: 1,
-scrollTrigger: {
-  scroller: "body",
-  trigger: ".footer-top",
-  start: "top 50%",
-  end: "top 45%",
-  scrub: 1,
-}
-})
 
 
 
@@ -250,18 +274,37 @@ gsap.from(".circle", {
     scroller: "body",
     trigger: ".category-page",
     start: "top 90%"
-    // end: "top 80%",
-    // scrub: 2
   }
-  // scrollTrigger: {
-  //   scroller: "body",
-  //   trigger: ".category-page",
-  //   markers: true,
-  //   start: "top 95%",
-  //   end: "top 80%",
-  //   scrub: 2
-  // }
 })
+
+document.querySelector(".category-page").addEventListener("mousemove", function(event){
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  positionX = (event.clientX/width) - 0.55;
+  positionY = (event.clientY/height) - 0.55;
+  gsap.to(".first img", { 
+      rotationY: positionX * 25,
+      rotationX: positionY * 25,
+      ease: "none"
+  });
+})
+
+let catHoverSound = document.querySelector(".category-panel-hover");
+categories.forEach(function(cat){
+  cat.addEventListener("mousemove", function(){
+    console.log("hoverinng")
+    catHoverSound.play();
+  })
+})
+
+function stopSongs() {
+  catHoverSound.pause();
+  catHoverSound.currentTime = 0;
+}
+
+
+
+
 
 
 let swiper = document.querySelector(".swiper");
@@ -275,12 +318,7 @@ videoBtn.addEventListener("click", function(){
     videoBtn.classList.remove("ri-play-circle-fill");
     videoBtn.classList.add("ri-close-circle-fill")
     
-    // swiper.style.top = "-100vh";
     overlay.style.display = "none";
-    // closeBtn.style.display = "block";
-    // gsap.to(".close-btn", {
-      //   top: "15%"
-      // })
       gsap.to(swiper, {
         top: "-100vh"
       })
@@ -296,16 +334,6 @@ videoBtn.addEventListener("click", function(){
       playToggle = 0;
     }
 })
-
-// closeBtn.addEventListener("click", function(){
-//   overlay.style.display = "block";
-//     gsap.to(".close-btn", {
-//       top: "-15%"
-//     })
-//     gsap.to(swiper, {
-//       top: "50%"
-//     })
-// })
 
 
 
@@ -349,6 +377,72 @@ function slider() {
 slider();
 
 
+
+
+
+
+// STEAM GIFT CARDS SECTION 
+
+
+
+function giftCardsAnimation(){
+  let leftContCards = document.querySelectorAll(".gift-cards");
+  let leftCardsArr = [...leftContCards];
+  
+  function updateCards() {
+      leftCardsArr.forEach(e => {
+          e.classList.remove("card1");
+          e.classList.remove("card2");
+          e.classList.remove("card3");
+          e.classList.remove("card4");
+          e.classList.remove("card5");
+          e.classList.remove("card6");
+      });
+      leftCardsArr.forEach((card, idx) => {
+          card.classList.add(`card${idx+1}`);
+      })
+  }
+  
+  function animateLeftCards() {
+      leftCardsArr.push(leftCardsArr.shift());
+      // leftCardsArr.unshift(leftCardsArr.pop());
+      updateCards();
+  }
+  
+  
+  
+  
+  let rightContCards = document.querySelectorAll(".gift-rcards");
+  let rightCardsArr = [...rightContCards];
+  
+  function updateRCards() {
+      rightCardsArr.forEach(e => {
+          e.classList.remove("rcard1");
+          e.classList.remove("rcard2");
+          e.classList.remove("rcard3");
+          e.classList.remove("rcard4");
+          e.classList.remove("rcard5");
+          e.classList.remove("rcard6");
+      });
+      rightCardsArr.forEach((card, idx) => {
+          card.classList.add(`rcard${idx+1}`);
+      })
+  }
+  
+  function animateRightCards() {
+      rightCardsArr.push(rightCardsArr.shift());
+      // rightCardsArr.unshift(rightCardsArr.pop());
+      updateRCards();
+  }
+  
+  setInterval(()=>{
+      animateLeftCards();
+      animateRightCards();
+  }, 900);
+  
+}
+
+giftCardsAnimation();
 
 
 
@@ -399,50 +493,6 @@ naveCloseBtn.addEventListener("click", function(){
 
 
 
-// gsap.to(".category-page", {
-//   y: "-100vh",
-//   scrollTrigger: {
-//     markers: true,
-//     scroller: "body",
-//     trigger: ".category-page",
-//     start: "top 90%",
-//     end: "top 60%",
-//     scrub: 1,
-//   }
-// })
-
-
-
-
-// Explore page 
-let exploreTitle = document.querySelector(".explore-title");
-gsap.from(exploreTitle, {
-  y: -800,
-  opacity: 0,
-  scrollTrigger: {
-    scroller: "body",
-    trigger: ".explore-page",
-    start: "top 60%",
-    end: "top 50%",
-    scrub: 1
-  }
-})
-
-
-exploreTl = gsap.timeline();
-exploreTl.from(".cards", {
-  scale: 0,
-  duration: 1,
-  stagger: .4,
-  scrollTrigger: {
-    scroller: "body",
-    trigger: ".explore-page",
-    start: "top 20%",
-    end: "top -10%",
-    scrub: 4
-  }
-})
-
 
 gsap.from(".offer-title h1", {
   x: -800,
@@ -456,22 +506,4 @@ gsap.from(".offer-title h1", {
   }
 })
 
-
-
-
-
-
-// gsap.to(".explore-page img", {
-//   y: -400,
-//   duration: 2,
-//   stagger: 1,
-//   scrollTrigger: {
-//     scroller: "body",
-//     trigger: ".explore-page",
-//     markers: true,
-//     start: "top 20%",
-//     end: "top 100%",
-//     scrub: 5,
-//   }
-// })
 
